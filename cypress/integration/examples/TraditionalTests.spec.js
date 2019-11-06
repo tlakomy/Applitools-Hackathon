318688d29868ceb84c3172249d1b87cc2403913d
Tomasz Łakomy <tomasz.lakomy@olx.pl>
2019-11-06 21:15:56 +0100

    Added tests for login functionality

--- cypress/integration/examples/TraditionalTests.spec.js
+++ cypress/integration/examples/TraditionalTests.spec.js
@@ -1,46 +1,106 @@
 /// <reference types="Cypress" />
 
+import { selectors as loginSelectors } from '../../selectors/login';
+
+const {
+    loginLogo,
+    loginHeader,
+    loginUsernameIcon,
+    loginPasswordIcon,
+    loginUsernameInput,
+    loginPasswordInput,
+    loginButton,
+    loginRememberMeLabel,
+    loginSocialMediaButtons,
+    loginAlertWarning
+} = loginSelectors;
+
 context('Applitools Hackathon - traditional tests V1', () => {
     beforeEach(() => {
         cy.visit('https://demo.applitools.com/hackathon.html');
     });
 
-    it('Should contain all necessary login page UI elements', () => {
-        // Verify the page header
-        cy.get('.logo-w img').should('have.attr', 'src', 'img/logo-big.png');
-        cy.get('.auth-header').contains('Login Form');
+    context('UI elements', () => {
+        it('Should contain all necessary login page UI elements', () => {
+            // Verify the page header
 
-        // Verify if icons are displayed
-        cy.get('.os-icon-user-male-circle');
-        cy.get('.os-icon-fingerprint');
+            cy.get(loginLogo).should('have.attr', 'src', 'img/logo-big.png');
+            cy.get(loginHeader).contains('Login Form');
 
-        // Verify the login form
-        cy.get('label').contains('Username');
-        cy.get('input#username').should(
-            'have.attr',
-            'placeholder',
-            'Enter your username'
-        );
+            // Verify if icons are displayed
+            cy.get(loginUsernameIcon);
+            cy.get(loginPasswordIcon);
 
-        cy.get('label').contains('Password');
-        cy.get('input#password').should(
-            'have.attr',
-            'placeholder',
-            'Enter your password'
-        );
+            // Verify the login form
+            cy.get('label').contains('Username');
+            cy.get(loginUsernameInput).should(
+                'have.attr',
+                'placeholder',
+                'Enter your username'
+            );
 
-        cy.get('#log-in').contains('Log In');
-        cy.get('.form-check-label').contains('Remember Me');
+            cy.get('label').contains('Password');
+            cy.get(loginPasswordInput).should(
+                'have.attr',
+                'placeholder',
+                'Enter your password'
+            );
 
-        // Verify the social media links
-        cy.get('.buttons-w img')
-            .eq(0)
-            .should('have.attr', 'src', 'img/social-icons/twitter.png');
-        cy.get('.buttons-w img')
-            .eq(1)
-            .should('have.attr', 'src', 'img/social-icons/facebook.png');
-        cy.get('.buttons-w img')
-            .eq(2)
-            .should('have.attr', 'src', 'img/social-icons/linkedin.png');
+            cy.get(loginButton).contains('Log In');
+            cy.get(loginRememberMeLabel).contains('Remember Me');
+
+            // Verify the social media links
+            cy.get(loginSocialMediaButtons)
+                .eq(0)
+                .should('have.attr', 'src', 'img/social-icons/twitter.png');
+            cy.get(loginSocialMediaButtons)
+                .eq(1)
+                .should('have.attr', 'src', 'img/social-icons/facebook.png');
+            cy.get(loginSocialMediaButtons)
+                .eq(2)
+                .should('have.attr', 'src', 'img/social-icons/linkedin.png');
+        });
+    });
+
+    context('Login functionality', () => {
+        it('Should show an error if users tries to log in without providing the username and password', () => {
+            cy.get(loginButton).click();
+
+            cy.get(loginAlertWarning).should(
+                'contain.text',
+                'Both Username and Password must be present'
+            );
+        });
+
+        it('Should show an error if users tries to log in without providing a password', () => {
+            cy.get(loginUsernameInput).type('admin');
+            cy.get(loginButton).click();
+
+            cy.get(loginAlertWarning).should(
+                'contain.text',
+                'Password must be present'
+            );
+        });
+
+        it('Should show an error if users tries to log in without providing a username', () => {
+            cy.get(loginPasswordInput).type('superSecretPassw0rd');
+            cy.get(loginButton).click();
+
+            cy.get(loginAlertWarning).should(
+                'contain.text',
+                'Username must be present'
+            );
+        });
+
+        it('Should allow the user to log in after providing username and password', () => {
+            cy.get(loginUsernameInput).type('admin');
+            cy.get(loginPasswordInput).type('superSecretPassw0rd');
+            cy.get(loginButton).click();
+
+            cy.url().should(
+                'equal',
+                'https://demo.applitools.com/hackathonApp.html'
+            );
+        });
     });
 });
---
+++ cypress/selectors/login.js
@@ -0,0 +1,14 @@
+export default {
+    selectors: {
+        loginLogo: '.logo-w img',
+        loginHeader: '.auth-header',
+        loginUsernameIcon: '.os-icon-user-male-circle',
+        loginPasswordIcon: '.os-icon-fingerprint',
+        loginUsernameInput: 'input#username',
+        loginPasswordInput: 'input#password',
+        loginButton: 'button#log-in',
+        loginRememberMeLabel: '.form-check-inline .form-check-label',
+        loginSocialMediaButtons: '.buttons-w img',
+        loginAlertWarning: '.alert-warning'
+    }
+};
